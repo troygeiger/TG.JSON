3b788f4fe4e31f1d0469627ca5c9a3087625de33
Troy Geiger <troygeiger@users.noreply.github.com>
2022-01-27 11:03:11 -0500

    Added more NUnit tests
    The following tests were added
    - ConversionTests for testing various type conversions.
    - JsonBinaryTests for testing JsonBinary cloning

---
+++ TG.JSON.Tests/ConversionTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace TG.JSON.Tests
+{
+    [TestFixture]
+    public class ConversionTests
+    {
+
+        /// <summary>
+        /// If nothing throws an exception, I consider that a pass.
+        /// </summary>
+        [Test]
+        [TestCase(5)]
+        [TestCase(true)]
+        [TestCase("Text")]
+        public void TestJsonValueToInt(object src)
+        {
+            JsonValue value = JsonValue.GetValueFromObject(src);
+            int intVal = value;
+        }
+
+        /// <summary>
+        /// If nothing throws an exception, I consider that a pass.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="target"></param>
+        [Test]
+        [TestCase(new byte[] {0,3,2,5}, typeof(byte[]))]
+        [TestCase(true, typeof(int))]
+        [TestCase(null, typeof(bool))]
+        public void TestChangeType(object data, Type target)
+        {
+            JsonValue json = JsonValue.GetValueFromObject(data);
+            object backTo = Convert.ChangeType(json, target);
+        }
+
+        
+    }
+
+   
+}
---
+++ TG.JSON.Tests/JsonBinaryTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TG.JSON;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace TG.JSON.Tests
+{
+    [TestFixture()]
+    public class JsonBinaryTests
+    {
+        [Test()]
+        public void CloneTest()
+        {
+            JsonBinary binary = new JsonBinary(new byte[] { 1, 2, 3, 4 });
+            JsonBinary copy = binary.Clone() as JsonBinary;
+            Assert.AreEqual(binary.Value, copy.Value);
+        }
+    }
+}
